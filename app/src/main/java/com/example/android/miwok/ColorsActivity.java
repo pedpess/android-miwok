181df087ec5da6c400f7474d535530eaadc79eba
Pedro Pessoa <pedro.pessoa@concrete.com.br>
2016-09-13 17:12:38 -0300

    Add colors list

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -28,6 +28,27 @@ public class ColorsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+
+
+        WordAdapter adapter =
+                new WordAdapter(this, words);
+
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
 
 
     }
